dc33d212cfb15fc013add4201953ad3ab7103c66
bxp6666 <bjpicciano@gmail.com>
2018-10-31 13:02:27 -0400

    started editor functionality and fixed caption json

---
+++ client/editor.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+}
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '',
+        width: '',
+        videoId: 'YE7VzlLtp-4',
+        events: {
+            'onReady': () => {},
+            'onStateChange': () => {}
+        }
+    });
+}
